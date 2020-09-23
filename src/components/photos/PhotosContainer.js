803ef9b83f4b28875157018aff65207d246811c6
Daniel Páez <danielpaezsw66@gmail.com>
2020-09-22 23:54:24 -0300

    Initial query done using fetch

--- src/components/photos/PhotosContainer.js
+++ src/components/photos/PhotosContainer.js
@@ -1,20 +1,58 @@
-import React, { Component, Fragment } from 'react';
+import React, { Component, Fragment } from "react";
 
- class PhotosContainer extends Component {
+class PhotosContainer extends Component {
+	state = {
+		photos: [],
+	};
 
- 	constructor(props) {
- 		super(props)
-
- 		this.state = {
-
- 		}
- 	}
-
+	componentDidMount = (props) => {
+		console.log("epale");
+		fetch("https://graphqlzero.almansi.me/api", {
+			method: "POST",
+			headers: { "content-type": "application/json" },
+			body: JSON.stringify({
+				query: `{
+		      photo(id: 1) {
+		        id
+		        url
+		        title
+		        album {
+		        	title
+		        }
+		      }
+		    }`,
+			}),
+		})
+			.then((res) => res.json())
+			.then((photos) => {
+				this.setState({
+					photos: [...this.state.photos, photos.data.photo],
+				});
+			});
+	};
 
 	render() {
-		return (
-			<Fragment>Hello PhotosContainer </Fragment>
-		);
+		console.log(this.state);
+		if (this.state.photos.length > 0) {
+			return (
+				<section>
+					{this.state.photos.map((photo) => (
+						<figure key={photo.id}>
+							<img
+								src={photo.url}
+								width="300"
+								alt={photo.title}
+								title={photo.title}
+							/>
+							<figcaption>
+								{photo.title} | Album: {photo.album.title}
+							</figcaption>
+						</figure>
+					))}
+				</section>
+			);
+		}
+		return <Fragment>Hello PhotosContainer </Fragment>;
 	}
 }
 
